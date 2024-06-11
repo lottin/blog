76b645762fe2799d954a3186a4ad30f538d1d91c
Ramish Benoit <lottinbudzi12@gmail.com>
2024-06-11 13:03:45 +0100

    Added the homepage and about pages simple structure

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #User defined applications
+    'blogApp.apps.BlogappConfig', 
 ]
 
 MIDDLEWARE = [
